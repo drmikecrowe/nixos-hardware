ddda5263a22ce865e92c5a0450ee9efa1905f5f2
Mike Crowe <drmikecrowe@gmail.com>
2023-07-08 15:07:12 -0400

    Another test for nvidia

--- dell/xps/15-9560/nvidia/default.nix
+++ dell/xps/15-9560/nvidia/default.nix
@@ -9,4 +9,12 @@
 
   services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
   boot.extraModulePackages = lib.mkDefault [ pkgs.linuxPackages.nvidia_x11 ];
+
+  hardware.nvidia.prime = {
+    # Bus ID of the Intel GPU.
+    intelBusId = lib.mkDefault "PCI:0:2:0";
+
+    # Bus ID of the NVIDIA GPU.
+    nvidiaBusId = lib.mkDefault "PCI:1:0:0";
+  };
 }
